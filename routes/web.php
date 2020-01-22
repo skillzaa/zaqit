9f332618b74fa9a8de8c63e31465ef4eda13d4d5
Bilal Tariq <bils32611@gmail.com>
2020-01-22 16:01:37 +0500

    Installed jwt but not implemented yet

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/help', function () {
-    return "Help on its way";
+Route::get('/debugbar', function () {
+    try {
+        throw new Exception('foobar');
+      } catch (Exception $e) {
+      //  Debugbar::addException($e);
+      }
+//...................................
+$arr = ["a","b","c"];
+Debugbar::info($arr);
+Debugbar::error('Error!');
+Debugbar::warning('Watch out…');
+Debugbar::addMessage('Another message', 'mylabel');
+//...................................
+
+
+    return "Debugbar";
 });
 
 ?>
